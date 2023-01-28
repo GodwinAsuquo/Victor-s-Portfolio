e06914ded9a09b554e4cd0d93f097e7d7a07418b
GodwinAsuquo <godwinedetauquo@gmail.com>
2023-01-28 15:55:11 +0100

    worked on the navbar background color

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,36 +1,54 @@
-import React, { createRef, useRef } from 'react'
-import Typed from 'react-typed';
-import { FiChevronDown, FiChevronRight  } from "react-icons/fi";
-import ProjectsIntroduction from './ProjectsIntroduction';
-
-
+import React, { createRef, useRef } from "react";
+import Typed from "react-typed";
+import { FiChevronDown, FiChevronRight } from "react-icons/fi";
+import ProjectsIntroduction from "./ProjectsIntroduction";
 
 const Hero = () => {
+  const ref = createRef();
 
-const ref = createRef()
-
-const handleClick = () => {
-    ref.current?.scrollIntoView({behavior: 'smooth'});
+  const handleClick = () => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
   };
 
   return (
-    <section id='hero' className='dark:bg-[#0D0D0D]'>
-      <div className='container mx-auto flex flex-col items-center py-48  dark:text-white'>
-         <p className='text-lg md:text-xl'>HELLO, NICE TO MEET YOU</p>
-        <h1 className='font-[800] mt-6 text-[32px] md:text-7xl  text-deepBlack dark:text-white'>I am <span><Typed strings={['Victor', 'Creative']} typeSpeed={120} backSpeed={140} loop/></span></h1>
-       
-        <p className='font-light sm:w-[85%] md:w-[45%] text-center text-[22.8px] mt-3 md:text-2xl leading-9'>A product designer based in Abuja, Nigeria.
-          Currently, I am designing engaging experiences for users in healthtech. Previously, I have worked as a product designer in the proptech and fintech industry.</p>
-          <button onClick={handleClick} className='hidden md:flex text-white dark:text-black mt-16 items-center text-lg bg-black dark:bg-[rgb(242,242,247)] px-8 py-4 space-x-2 rounded-[3px]'>
-            <p> See My Projects</p> <FiChevronDown/></button>
-            <button onClick={handleClick}  className='md:hidden text-white dark:bg-[rgb(242,242,247)] dark:text-black mt-16 flex items-center text-lg bg-black dark:bg- px-8 py-4 space-x-2 rounded-r-lg rounded-tl-lg'>
-            <p> See My Projects</p><FiChevronDown /></button>
+    <section id="hero" className="dark:bg-[#0D0D0D]">
+      <div className="container mx-auto flex flex-col items-center py-48  dark:text-white">
+        <p className="text-lg md:text-xl">HELLO, NICE TO MEET YOU</p>
+        <h1 className="font-[800] mt-6 text-[32px] md:text-7xl  text-deepBlack dark:text-white">
+          I am{" "}
+          <span>
+            <Typed
+              strings={["Victor", "Creative"]}
+              typeSpeed={120}
+              backSpeed={140}
+              loop
+            />
+          </span>
+        </h1>
+
+        <p className="m-auto font-light w-[85%] md:w-[45%] text-center text-[22.8px] mt-3 md:text-2xl leading-9">
+          A product designer based in Abuja, Nigeria. Currently, I am designing
+          engaging experiences for users in healthtech. Previously, I have
+          worked as a product designer in the proptech and fintech industry.
+        </p>
+        <button
+          onClick={handleClick}
+          className="hidden md:flex text-white dark:text-black mt-16 items-center text-lg bg-black dark:bg-[rgb(242,242,247)] px-8 py-4 space-x-2 rounded-[3px]"
+        >
+          <p> See My Projects</p> <FiChevronDown />
+        </button>
+        <button
+          onClick={handleClick}
+          className="md:hidden text-white dark:bg-[rgb(242,242,247)] dark:text-black mt-16 flex items-center text-lg bg-black dark:bg- px-8 py-4 space-x-2 rounded-r-lg rounded-tl-lg"
+        >
+          <p> See My Projects</p>
+          <FiChevronDown />
+        </button>
       </div>
 
-      <ProjectsIntroduction ref={ref}/>
-
+      <ProjectsIntroduction ref={ref} />
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/ProjectsIntroduction.js
+++ src/components/ProjectsIntroduction.js
@@ -1,96 +1,126 @@
-import wephco from '../assets/wephco2.png'
-import wellahealth from '../assets/wellahealth2.png'
-import epay from '../assets/epay2.png'
-import portfolio from '../assets/portfolio2.png'
-import chronicare from '../assets/chroniccare2.png'
-import { FiChevronRight } from 'react-icons/fi'
-import { Link } from 'react-router-dom'
-import { forwardRef, useRef } from 'react'
-
+import wephco from "../assets/wephco2.png";
+import wellahealth from "../assets/wellahealth2.png";
+import epay from "../assets/epay2.png";
+import portfolio from "../assets/portfolio2.png";
+import chronicare from "../assets/chroniccare2.png";
+import { FiChevronRight } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import { forwardRef, useRef } from "react";
 
 const ProjectsIntroduction = forwardRef((props, ref) => {
-
   return (
-    <section ref={ref}  className='dark:bg-[#0D0D0D] pb-36'>
-       <h2 className=' text-center dark:text-white font-bold text-2xl pb-10 pt-20'>Selected Projects</h2>
-       <div className='grid md:grid-cols-2 gap-5 px-14'>
+    <section ref={ref} className="dark:bg-[#0D0D0D] pb-36">
+      <h2 className=" text-center dark:text-white font-bold text-2xl pb-10 pt-20">
+        Selected Projects
+      </h2>
+      <div className="grid md:grid-cols-2 gap-5 px-14">
         {/* wephco  */}
-        <Link to='/wephco' >
-          <div className='wephco'>
-              <div className='flex flex-col justify-between'>
+        <Link to="/wephco">
+          <div className="relative flex bg-[#095069] h-[250px] md:h-[390px] w-[100%] rounded-lg p-5 text-white">
+            <div className="flex flex-col justify-between">
               <div>
-             <h3 className='font-semi-bold md:text-lg'>WEPHCO</h3>
-            <p className='text-sm font-light'>Real Estate</p>
+                <h3 className="font-semi-bold md:text-lg">WEPHCO</h3>
+                <p className="text-sm font-light">Real Estate</p>
               </div>
-        <p className='flex flex-row items-center text-xs md:text-base'>
-          See work<FiChevronRight/></p> 
+              <p className="flex flex-row items-center text-xs md:text-base">
+                See work
+                <FiChevronRight />
+              </p>
             </div>
-            <img className='absolute bottom-0 right-0 w-[75%]' src={wephco} alt='wephco image' />
-            </div>
-            </Link>
+            <img
+              className="absolute bottom-0 right-0 w-[75%]"
+              src={wephco}
+              alt="wephco image"
+            />
+          </div>
+        </Link>
 
-          {/* wella agent  */}
-          <Link to='/wella-agent' >
-            <div className='relative flex bg-[#4059AD] h-[250px] md:h-[390px] w-[100%] rounded-lg p-5 text-white '>
-              <div className='flex flex-col justify-between'>
+        {/* wella agent  */}
+        <Link to="/wella-agent">
+          <div className="relative flex bg-[#4059AD] h-[250px] md:h-[390px] w-[100%] rounded-lg p-5 text-white ">
+            <div className="flex flex-col justify-between">
               <div>
-             <h3 className='font-semi-bold md:text-lg'>WELLA AGENT</h3>
-            <p className='text-sm font-light'>Health & Wellness</p>
+                <h3 className="font-semi-bold md:text-lg">WELLA AGENT</h3>
+                <p className="text-sm font-light">Health & Wellness</p>
               </div>
-        <p className='flex flex-row items-center text-xs md:text-base'>
-          See work<FiChevronRight/></p> 
+              <p className="flex flex-row items-center text-xs md:text-base">
+                See work
+                <FiChevronRight />
+              </p>
             </div>
-            <img className='absolute bottom-0 right-[8%] w-[55%]' src={wellahealth} alt='wephco image' />
-            </div>
-            </Link>
+            <img
+              className="absolute bottom-0 right-[8%] w-[55%]"
+              src={wellahealth}
+              alt="wephco image"
+            />
+          </div>
+        </Link>
 
-            {/* epay  */}
-            <Link to='/epay'>
-            <div className='relative flex bg-[#465055] h-[250px] md:h-[390px] w-[100%] rounded-lg p-5 text-white '>
-              <div className='flex flex-col justify-between'>
+        {/* epay  */}
+        <Link to="/epay">
+          <div className="relative flex bg-[#465055] h-[250px] md:h-[390px] w-[100%] rounded-lg p-5 text-white ">
+            <div className="flex flex-col justify-between">
               <div>
-             <h3 className='font-semi-bold md:text-lg'>EPAY</h3>
-            <p className='text-sm font-light'>Fintech App</p>
+                <h3 className="font-semi-bold md:text-lg">EPAY</h3>
+                <p className="text-sm font-light">Fintech App</p>
               </div>
-        <p className='flex flex-row items-center text-xs md:text-base'>
-          See work<FiChevronRight/></p>
+              <p className="flex flex-row items-center text-xs md:text-base">
+                See work
+                <FiChevronRight />
+              </p>
             </div>
-            <img className='absolute bottom-0 right-[8%] w-[55%]' src={epay} alt='wephco image' />
-            </div>
-            </Link>
+            <img
+              className="absolute bottom-0 right-[8%] w-[55%]"
+              src={epay}
+              alt="wephco image"
+            />
+          </div>
+        </Link>
 
-            {/* my portfolio  */}
-            <Link to='/portfolio'>
-            <div className='relative flex bg-[#094440] h-[250px] md:h-[390px] w-[100%] rounded-lg p-5 text-white '>
-              <div className='flex flex-col justify-between'>
+        {/* my portfolio  */}
+        <Link to="/portfolio">
+          <div className="relative flex bg-[#094440] h-[250px] md:h-[390px] w-[100%] rounded-lg p-5 text-white ">
+            <div className="flex flex-col justify-between">
               <div>
-             <h3 className='font-semi-bold md:text-lg'>MY PORTFOLIO</h3>
-            <p className='text-sm font-light'>Work Showcase</p>
+                <h3 className="font-semi-bold md:text-lg">MY PORTFOLIO</h3>
+                <p className="text-sm font-light">Work Showcase</p>
               </div>
-        <p className='flex flex-row items-center text-xs md:text-base'>
-          See work<FiChevronRight/></p>
+              <p className="flex flex-row items-center text-xs md:text-base">
+                See work
+                <FiChevronRight />
+              </p>
             </div>
-            <img className='absolute bottom-0 right-0 w-[72%] md:w-[75%]' src={portfolio} alt='wephco image' />
-            </div>
-            </Link>
+            <img
+              className="absolute bottom-0 right-0 w-[72%] md:w-[75%]"
+              src={portfolio}
+              alt="wephco image"
+            />
+          </div>
+        </Link>
 
-            {/* chronicare  */}
-            <Link to='/chronicare'>
-            <div className='relative flex bg-[#1489C8] h-[250px] md:h-[390px] w-[100%] rounded-lg p-5 text-white '>
-              <div className='flex flex-col justify-between'>
+        {/* chronicare  */}
+        <Link to="/chronicare">
+          <div className="relative flex bg-[#1489C8] h-[250px] md:h-[390px] w-[100%] rounded-lg p-5 text-white ">
+            <div className="flex flex-col justify-between">
               <div>
-             <h3 className='font-semi-bold md:text-lg'>CHRONICARE</h3>
-            <p className='text-sm font-light'>Health & Wellness</p>
+                <h3 className="font-semi-bold md:text-lg">CHRONICARE</h3>
+                <p className="text-sm font-light">Health & Wellness</p>
               </div>
-        <p className='flex flex-row items-center text-xs md:text-base'>
-          See work<FiChevronRight/></p>
+              <p className="flex flex-row items-center text-xs md:text-base">
+                See work
+                <FiChevronRight />
+              </p>
             </div>
-            <img className='absolute bottom-0 right-0 w-[71%] md:w-[75%]' src={chronicare} alt='wephco image' />
-            </div>
-            </Link>
-       </div>
+            <img
+              className="absolute bottom-0 right-0 w-[71%] md:w-[75%]"
+              src={chronicare}
+              alt="wephco image"
+            />
+          </div>
+        </Link>
+      </div>
     </section>
-  )
-})
+  );
+});
 
-export default ProjectsIntroduction
+export default ProjectsIntroduction;
